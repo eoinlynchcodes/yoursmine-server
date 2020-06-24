b93827ff20726e5490485b5f155a12fd2f80de34
eoinlynchcodes <eoin95lynch@gmail.com>
2020-06-24 08:48:06 +0100

    Changed index file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const server = require('./api/server');
+const app = require('./api/server');
 
 const PORT = process.env.PORT || 3333;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
 })
